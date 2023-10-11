8c64227dcff593c8ddfcd78190c6a39095ee513d
syha-studio <syauqillah0908@gmail.com>
2023-10-12 05:41:40 +0700

    Menambahkan pilihan Category PARA di fragment Notes

---
+++ app/src/main/java/com/example/secondbrain/ui/note/ModelCategory.kt
@@ -0,0 +1,9 @@
+package com.example.secondbrain.ui.note
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class ModelCategory (
+    var title : String? = null
+) : Parcelable
