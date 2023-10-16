113f26d03a7025402c6fd80777ff5a96b0589ae0
Revangga <revanggaprakusya527@gmail.com>
2023-10-17 00:13:09 +0700

    update

--- app/src/main/java/com/example/secondbrain/ui/capture/CaptureFragment.kt
+++ app/src/main/java/com/example/secondbrain/ui/capture/CaptureFragment.kt
@@ -1,4 +1,4 @@
-package com.example.secondbrain.ui.calendar
+package com.example.secondbrain.ui.capture
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,36 +7,36 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.secondbrain.databinding.FragmentCalendarBinding
+import com.example.secondbrain.databinding.FragmentCaptureBinding
 
 class CaptureFragment : Fragment() {
 
-    private var _binding: FragmentCalendarBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val captureViewModel =
-            ViewModelProvider(this)[CaptureViewModel::class.java]
-
-        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        captureViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+//    private var _binding: FragmentCaptureBinding? = null
+//
+//    // This property is only valid between onCreateView and
+//    // onDestroyView.
+//    private val binding get() = _binding!!
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val captureViewModel =
+//            ViewModelProvider(this)[CaptureViewModel::class.java]
+//
+//        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//
+//        val textView: TextView = binding.textNotifications
+//        captureViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+//        return root
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 }
--- app/src/main/java/com/example/secondbrain/ui/capture/CaptureViewModel.kt
+++ app/src/main/java/com/example/secondbrain/ui/capture/CaptureViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.secondbrain.ui.calendar
+package com.example.secondbrain.ui.capture
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class CaptureViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Calendar Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+//class CaptureViewModel : ViewModel() {
+//
+//    private val _text = MutableLiveData<String>().apply {
+//        value = "This is Calendar Fragment"
+//    }
+//    val text: LiveData<String> = _text
+//}
